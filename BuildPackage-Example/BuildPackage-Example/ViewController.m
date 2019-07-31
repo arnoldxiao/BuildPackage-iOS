11e7913c72f3ba83d66cdd51c8b9f98258091f0a
arnoldxiao <arnoldxiao@163.com>
2019-07-31 18:57:56 +0800

    1.0.0版本提交

--- BuildPackage-Example/BuildPackage-Example/ViewController.m
+++ BuildPackage-Example/BuildPackage-Example/ViewController.m
@@ -17,6 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UILabel *versionLabel = [[UILabel alloc] init];
+    versionLabel.font = [UIFont systemFontOfSize:20.0f];
+    versionLabel.textColor = UIColor.whiteColor;
+    versionLabel.backgroundColor = UIColor.grayColor;
+    versionLabel.text = [NSString stringWithFormat:@"版本号: %@", [self getAppVersion]];
+    
+    [self.view addSubview:versionLabel];
+    
+    [versionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+    }];
+}
+
+- (NSString *)getAppVersion {
+    NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    NSString *build = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+    return [NSString stringWithFormat:@"%@-%@", version, build];
 }
 
 
